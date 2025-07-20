50a184cb108841282cf8d8cb7db35ef0d2edd0ed
moneesh <moneesh.reddy@tmdc.io>
2025-07-20 15:02:51 +0530

    samples

--- terraform/securityhub.tf
+++ terraform/securityhub.tf
@@ -1,13 +1,45 @@
-module "security_hub" {
-  source = "terraform-aws-modules/security-hub/aws"
+resource "aws_securityhub_account" "main" {}
 
-  enable_security_hub = true
-  enable_guardduty    = true
-  enable_inspector    = true
-  enable_config       = true
+resource "aws_securityhub_standards_subscription" "cis" {
+  standards_arn = "arn:aws:securityhub:::standards/cis-aws-foundations-benchmark/v/1.2.0"
+}
 
-  security_hub_standards = [
-    "cis-aws-foundations-benchmark",
-    "aws-foundational-security-best-practices"
-  ]
+resource "aws_securityhub_standards_subscription" "aws_best_practices" {
+  standards_arn = "arn:aws:securityhub:::standards/aws-foundational-security-best-practices/v/1.0.0"
+}
+
+resource "aws_guardduty_detector" "main" {
+  enable = true
+}
+
+resource "aws_inspector2_enabler" "main" {
+  account_ids = [data.aws_caller_identity.current.account_id]
+  resource_types = ["EC2", "ECR", "LAMBDA"]
+}
+
+resource "aws_iam_role" "config" {
+  name = "aws-config-recorder-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "config.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "config_policy" {
+  role       = aws_iam_role.config.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
+
+resource "aws_config_configuration_recorder" "main" {
+  name     = "default"
+  role_arn = aws_iam_role.config.arn
 }
