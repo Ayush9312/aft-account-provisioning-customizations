ae83a98d836658906ec8a5e846e38818584bf6d8
moneesh <moneesh.reddy@tmdc.io>
2025-07-20 15:13:33 +0530

    sample

--- terraform/securityhub.tf
+++ terraform/securityhub.tf
@@ -4,13 +4,14 @@ resource "aws_securityhub_account" "main" {}
 
 resource "aws_securityhub_standards_subscription" "cis" {
   depends_on    = [aws_securityhub_account.main]
-  standards_arn = "arn:aws:securityhub:::standards/cis-aws-foundations-benchmark/v/1.2.0"
+  standards_arn = "arn:aws:securityhub:eu-central-1::standards/cis-aws-foundations-benchmark/v/1.2.0"
 }
 
 resource "aws_securityhub_standards_subscription" "aws_best_practices" {
   depends_on    = [aws_securityhub_account.main]
-  standards_arn = "arn:aws:securityhub:::standards/aws-foundational-security-best-practices/v/1.0.0"
+  standards_arn = "arn:aws:securityhub:eu-central-1::standards/aws-foundational-security-best-practices/v/1.0.0"
 }
+
 
 resource "aws_guardduty_detector" "main" {
   enable = true
